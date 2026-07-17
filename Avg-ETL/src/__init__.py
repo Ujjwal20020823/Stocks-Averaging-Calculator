e17ed84154f206e8882784295fd341c9f705734e
Ujjwal Shrestha <ujjwal.819531@som.tu.edu.np>
2026-07-17 21:11:18 +0545

    first commit

---
+++ Avg-ETL/src/__init__.py
@@ -0,0 +1,8 @@
+"""
+Smart Recovery Investment Engine - Core Application Package
+Version: 1.0.0
+Purpose: Help investors to make data-driven decision on averaging down postions
+"""
+
+__version__ = "1.0.0"
+__author__ = "Dhyeba Finance Project"
